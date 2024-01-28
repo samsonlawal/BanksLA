6769b1f674ecdc18598099953e3f60e37f321640
Samson <lawal4211@gmail.com>
2024-01-28 12:15:29 +0100

    Start

---
+++ src/UI/Navbar.jsx
@@ -0,0 +1,28 @@
+export default function Navbar() {
+  return (
+    <div className="">
+      {/* Logo */}
+      <img src="" alt="" />
+
+      {/* Links */}
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>List your Property</li>
+          <li>Locations</li>
+        </ul>
+      </div>
+
+      {/* Functionalities */}
+      <div>
+        <ul>
+          <li>Currency</li>
+          <li>Log In</li>
+          <li>Sign Up</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
