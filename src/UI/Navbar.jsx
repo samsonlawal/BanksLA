dff19967a43f765fd55a0448cef6d3bf1f5f4a27
Samson <lawal4211@gmail.com>
2024-01-28 20:19:52 +0100

    navbar

--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -1,27 +1,38 @@
+import logo from "../Images/banks.png";
+
 export default function Navbar() {
   return (
-    <div className="">
-      {/* Logo */}
-      <img src="" alt="" />
-
-      {/* Links */}
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact Us</li>
-          <li>List your Property</li>
-          <li>Locations</li>
-        </ul>
+    <div className=" w-full px-6 py-6 bg-white flex flex-row ">
+      <div className="w-30 h-full flex justify-center items-center">
+        <img src={logo} alt="" />
       </div>
 
-      {/* Functionalities */}
-      <div>
-        <ul>
-          <li>Currency</li>
-          <li>Log In</li>
-          <li>Sign Up</li>
-        </ul>
+      {/* links ad buttons */}
+      <div className="w-full flex flex-row justify-between px-16">
+        {/* Logo */}
+
+        {/* Links */}
+        <div>
+          <ul className="h-full flex space-x-10 justify-center items-center">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+            <li>List your Property</li>
+          </ul>
+        </div>
+
+        <button className="mx-20 text-primary bg-secondary py-2 px-6 hover:text-white hover:bg-primary">
+          Locations
+        </button>
+
+        {/* Functionalities */}
+        <div>
+          <ul className="h-full flex space-x-12 justify-center items-center px-10">
+            <li className="">Currency:</li>
+            <li>Log In</li>
+            <li className="border-2 border-black py-2 px-6">Sign Up</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F04D22",
+        secondary: "#FFF4F1",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
